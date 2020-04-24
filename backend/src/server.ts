bd7b25d870988fdf3b5f69fa70121c7a21a294bf
Donovan Pechetti <dovi_pf@hotmail.com>
2020-04-24 11:16:41 -0300

    sending updates on back-end

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
