79be1bc6d15274f754077e34948be9777824928b
Donovan Pechetti <dovi_pf@hotmail.com>
2020-04-27 21:33:54 -0300

    Structure and standards

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
