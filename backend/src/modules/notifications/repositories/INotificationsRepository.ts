7283a62365c064027dc1969a56411ca11d6c4505
Donovan Pechetti <dovi_pf@hotmail.com>
2020-05-18 15:13:31 -0300

    notification structure

--- backend/src/modules/notifications/repositories/INotificationsRepository.ts
+++ backend/src/modules/notifications/repositories/INotificationsRepository.ts
@@ -1,5 +1,5 @@
-import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
 import Notification from '@modules/notifications/infra/typeorm/schemas/Notification';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
 
 export default interface INotificationRepository {
   create(data: ICreateNotificationDTO): Promise<Notification>;
