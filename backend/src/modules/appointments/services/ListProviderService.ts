ed4e5baab834c4d33a241eb93eb63f1d415073c6
Donovan Pechetti <dovi_pf@hotmail.com>
2020-05-21 17:45:15 -0300

    list of providers cache

--- backend/src/modules/appointments/services/ListProviderService.ts
+++ backend/src/modules/appointments/services/ListProviderService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -12,14 +13,27 @@ class ListProfileService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
-    return user;
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      console.log('Query feita');
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
+
+    return users;
   }
 }
 
