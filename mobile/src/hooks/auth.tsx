82c99c8b5930bf6baac35501b9bb07f8ff3bed4a
Donovan Pechetti <dovi_pf@hotmail.com>
2020-06-23 16:46:09 -0300

    dashboard header

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -9,9 +9,16 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SingInCredentials {
@@ -20,7 +27,7 @@ interface SingInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SingInCredentials): Promise<void>;
   singOut(): void;
