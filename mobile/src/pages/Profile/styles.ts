785bfb0a53a35f0a8da2bcf6acc62f6cc593b910
Donovan Pechetti <dovi_pf@hotmail.com>
2020-07-17 18:59:56 -0300

    fix

--- mobile/src/pages/Profile/styles.ts
+++ mobile/src/pages/Profile/styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components/native';
 import { Platform } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
